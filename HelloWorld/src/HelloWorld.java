b41fb99a2b6ae7e2c860cc1e2e3734316f882f13
Daniel Sonnleitner <Sonnleitner.Daniel@t-online.de>
2014-10-26 21:12:00 +0100

    another test

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is git!\n");
-		System.out.println("Hello World\n");
+		System.out.println("Hello World - another test\n");
 	}
 
 }
