a0afc8efd68df3ba94c0e9cc9bc76041a11d44aa
Daniel Eichinger <Dany.Eichinger@gmail.com>
2014-11-10 15:09:36 +0100

    Prototyp überarbeitet + Erste Daten aus DB

---
+++ ABCAnalyse/src/datasource/dbConnector.java
@@ -0,0 +1,36 @@
+package datasource;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class dbConnector {
+
+	private static Connection connection = null; 
+	
+	public static Connection connectSqLite()
+	{  
+	     try 
+	     {  
+	         Class.forName("org.sqlite.JDBC");  
+	         connection = DriverManager.getConnection("jdbc:sqlite:SwDB.db");  
+	         return connection;
+	     } 
+	     catch (Exception e) 
+	     {  
+	         e.printStackTrace();  
+	         return null;
+	     }
+	}
+	
+	public static void closSqLiteConnection()
+	{
+		try
+		{
+			connection.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ABCAnalyse/src/datasource/selectBefehle.java
@@ -0,0 +1,7 @@
+package datasource;
+
+public class selectBefehle {
+
+	public static String selectABCEinteilung = "select * from ABCEinteilung order by Bezeichnung asc";
+	
+}
