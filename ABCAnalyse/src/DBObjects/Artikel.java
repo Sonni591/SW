50113314c942cf6d52aa918228a0bf6baf9fb2fb
michael spannbauer <michael.spannbauer@live.de>
2014-11-19 12:46:21 +0100

    x

---
+++ ABCAnalyse/src/DBObjects/Artikel.java
@@ -0,0 +1,8 @@
+package DBObjects;
+
+public class Artikel {
+ public String ArtikelNr;
+ public String Bezeichnung;
+ public double EKPreis;
+ public int WGNr;
+}
--- ABCAnalyse/src/logic/ABCRechnung.java
+++ ABCAnalyse/src/logic/ABCRechnung.java
@@ -5,10 +5,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+
 import DBObjects.Artikel;
 import datasource.CrudBefehle;
 import datasource.CrudFunktionen;
-import datasource.DBConnector;
+
 
 public class ABCRechnung {
 	private static Connection DBconnection = null;
@@ -16,7 +17,7 @@ public class ABCRechnung {
 	
 	public void getData(Connection _DBconnection){
 		DBconnection = _DBconnection;
-		artikellist = new ArrayList();
+		artikellist = new ArrayList<Artikel>();
 		try {
 			ResultSet daten = CrudFunktionen.getResult(DBconnection, CrudBefehle.selectDaten);
 			
@@ -24,20 +25,10 @@ public class ABCRechnung {
 				Artikel a = new Artikel();
 				a.Bezeichnung = daten.getString("Bezeichnung");
 				a.ArtikelNr = daten.getString("ArtikelNr");
-				//a.EKPreis = daten.get
+				a.EKPreis = (daten.getDouble("EKPreis"));
 				a.WGNr = daten.getInt("WGNr");
 				artikellist.add(a);
-			}
-			int i = 0;
-			System.out.println("-------");
-			for(Artikel a: artikellist){
-				System.out.println(a.ArtikelNr + " " + a.Bezeichnung + " "  + a.EKPreis + "\n");
-				i++;
-				if(i>100)
-					break;
-			}
-			ResultSet d = CrudFunktionen.getResult(DBconnection, CrudBefehle.test);
-			
+			}			
 		} catch(SQLException e) {
 			e.printStackTrace();
 		}
