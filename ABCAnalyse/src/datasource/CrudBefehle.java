a89641ce47c45292b109344d73c75bc721fec40a
Timo D. <denzintimo@web.de>
2014-11-19 14:41:16 +0100

    Befehl hinzugefügt

--- ABCAnalyse/src/datasource/CrudBefehle.java
+++ ABCAnalyse/src/datasource/CrudBefehle.java
@@ -5,6 +5,7 @@ public class CrudBefehle {
 	//Bereich für Select-Befehle
 	public static String selectABCEinteilung = "select * from ABCEinteilung order by Bezeichnung asc";
 	public static String selectAbsatz = "select * from Absatz";
+	public static String selectUmsatzGrouped = "SELECT ArtikelNr, Sum(Umsatz) as \"Gesamt Umsatz \"FROM Absatz GROUP BY ArtikelNr Order By \"Gesamt Umsatz\" DESC;";
 
 	//Bereich für Update-Befehle
 	public static String updateEinteilungAnteilA = "update " + "ABCEinteilung set AnteilA = ? where Bezeichnung = ?";
